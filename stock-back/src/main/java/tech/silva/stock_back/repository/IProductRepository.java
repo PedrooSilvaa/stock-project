f924ec91afc7fd8a08ac9fc3575b72e90e7f2cd4
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-11-10 12:30:22 -0300

    feat: create 3 methods in ProductService

--- stock-back/src/main/java/tech/silva/stock_back/repository/IProductRepository.java
+++ stock-back/src/main/java/tech/silva/stock_back/repository/IProductRepository.java
@@ -1,7 +1,13 @@
 package tech.silva.stock_back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import tech.silva.stock_back.entity.Product;
 
+import java.util.List;
+
 public interface IProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("select p from Product p where name like %name%")
+    List<Product> findAllByName(String name);
 }
--- stock-back/src/main/java/tech/silva/stock_back/service/ProductService.java
+++ stock-back/src/main/java/tech/silva/stock_back/service/ProductService.java
@@ -1,7 +1,12 @@
 package tech.silva.stock_back.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tech.silva.stock_back.entity.Product;
 import tech.silva.stock_back.repository.IProductRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -10,5 +15,20 @@ public class ProductService {
 
     public ProductService(IProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public Product createProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Product> getByName(String name) {
+        return productRepository.findAllByName(name);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Product> getAll(){
+        return productRepository.findAll();
     }
 }
