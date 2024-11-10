db78bd9d35ce85746e9ed76df1fbe7b11bf7ccc7
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-11-10 12:17:22 -0300

    feat: connect with database

---
+++ stock-back/src/main/java/tech/silva/stock_back/StockBackApplication.java
@@ -0,0 +1,13 @@
+package tech.silva.stock_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockBackApplication.class, args);
+	}
+
+}
---
+++ stock-back/src/test/java/tech/silva/stock_back/StockBackApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.silva.stock_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
