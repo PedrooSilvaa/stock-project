b4a249f5e83a4b822747a4eb2a5aa8ec0983b4f7
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-11-10 12:19:53 -0300

    feat: create Product entity

---
+++ stock-back/src/main/java/tech/silva/stock_back/entity/Product.java
@@ -0,0 +1,60 @@
+package tech.silva.stock_back.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int amount;
+
+    private double value;
+
+    public Product(Long id, String name, int amount, double value) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.value = value;
+    }
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
